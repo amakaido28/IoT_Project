3b12cb30235aedf5d1f98961f4fa8e805c21743a
appesiti <72612568+appesiti@users.noreply.github.com>
2021-12-30 19:58:40 +0100

    Add files via upload
    Scrittura da bridge verso server - prova_post_CLIENT
    Lettura dal server - prova_post_SERVER

---
+++ prova_POST_server.py
@@ -0,0 +1,36 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import pymysql
+
+connection = pymysql.connect(host='ec2-18-192-245-67.eu-central-1.compute.amazonaws.com', user='as', password='sa', database='app_db', port=6033)
+print("Connected to:", connection.get_server_info())
+cursor = connection.cursor()
+cursor.execute("show tables")
+data = cursor.fetchall()
+print(data)
+
+with connection:
+    with connection.cursor() as cursor:
+        # Read a single record
+        query = "UPDATE `JJ` SET  (`presenza`=%s) WHERE `nome`=%s && `cognome`=%s"
+        cursor.execute(query,('','Ama','Kappa'))
+        result = cursor.fetchone()
+        print(result)
+
+
+""""
+ADDR = "localhost"
+PORT = 8000
+
+class RequestHandler(BaseHTTPRequestHandler):        
+    def do_POST(self):
+        print(self.path)
+        length = int(self.headers['Content-length'])
+        print(self.rfile.read1(length))
+        self.send_response(200, "OK")
+        self.end_headers()
+        self.wfile.write(bytes("serverdata",'utf-8'))
+
+print("[EXECUTING....]")
+httpd = HTTPServer((ADDR, PORT), RequestHandler)
+httpd.serve_forever()
+"""
