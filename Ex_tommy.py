e7a52019f4c1fb7e432cd674ddf6ec868d4ebb5f
MerollaThomas <33688653+MerollaThomas@users.noreply.github.com>
2022-01-24 16:06:59 +0100

    Delete Ex_tommy.py

--- Ex_tommy.py
+++
@@ -1,9 +0,0 @@
-import http.client as http
-
-
-conn = http.HTTPConnection("127.0.0.1:8081")
-conn.request("GET", "/Receiver", "55:55:55:55:55:55")
-response = conn.getresponse()
-msg = response.read(17).decode('utf-8')
-conn.close()
-print(msg)
