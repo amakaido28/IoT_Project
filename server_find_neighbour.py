0aa2957d838def92e29cfff324d3b18d3d81f173
Lorenzo Calvano <80829330+Lorenzo-Calvano@users.noreply.github.com>
2022-02-05 01:35:11 +0100

    Add files via upload

--- server_find_neighbour.py
+++ server_find_neighbour.py
@@ -1,52 +1,67 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from webbrowser import get
 import pymysql
+import requests
 
+
+port_server = 8081
 port_db = 6033
-host_db = "ec2-3-69-25-163.eu-central-1.compute.amazonaws.com"
+host = "ec2-3-69-25-163.eu-central-1.compute.amazonaws.com"
 user_db='as'
 pass_db='sa'
 database_name='app_db'
+bot_url = 'http://127.0.0.1:5000/vicino'
 
-addr_server = "ec2-3-69-25-163.eu-central-1.compute.amazonaws.com"
-port_server = 8001
 
 FORMAT = 'utf-8'
+
 message_default = '00:00:00:00:00:00'
+message_noresult = 'FF:FF:FF:FF:FF:FF'
 Mac_lenght = 17
 
-def get_receiver(id=message_default):
-    connection = pymysql.connect(host=host_db, user='as', password='sa', database='app_db', port=port_db)
 
+
+def send_message_bot(data_prop, data_neig):
+  chat_id_neig = str(data_neig[4]) 
+  chat_id_prop = str(data_prop[3])
+
+  DATA = { 'nome_prop' : data_prop[1], 'cognome_prop' : data_prop[2],
+            'chatID_prop' : chat_id_prop, 'nome_neig' : data_neig[1], 
+            'cognome_neig' : data_neig[2], 'ind_neig' : data_neig[3], 
+            'chatID_neig' : chat_id_neig}
+
+  response = requests.post(url=bot_url, data=DATA)
+  print(response)
+
+
+
+def get_receiver(id=message_default):
+    connection = pymysql.connect(host=host, user='as', password='sa', database='app_db', port=port_db)
     cursor = connection.cursor()
-    cursor.execute(f"SELECT presenza, nome, cognome, chatID FROM JJ WHERE MAC='{id}'")
+    
+    cursor.execute(f"SELECT presenza, nome, cognome, chatID, latitudine, longitudine FROM JJ WHERE MAC='{id}'")
     data_prop = cursor.fetchone()
 
     if(data_prop[0] == 1):
       connection.close()
       return message_default
-    
-    cursor.execute(f"SELECT MAC, nome, cognome, indirizzo, chatID FROM JJ WHERE MAC!='{id}' AND presenza=0")
+
+    lat_prop = data_prop[4]
+    long_prop = data_prop[5]
+    cursor.execute(f"SELECT MAC, nome, cognome, indirizzo, chatID FROM JJ WHERE MAC!='{id}' AND presenza=0 AND ST_Distance_Sphere(ST_GeomFromText('POINT({lat_prop} {long_prop})', 4326), ST_SRID(POINT(longitudine, latitudine), 4326))<750")
     data_neig = cursor.fetchone()
 
     if(data_neig == None):
       connection.close()
-      return message_default
+      return message_noresult
 
-    chat_id_neig = str(data_neig[4]) #non so se chatID è string o int 
-    chat_id_prop = str(data_prop[3])
-
-    message_neighbour = "Ti arriverà il pacco di " + str(data_prop[1]) + " " + str(data_prop[2])
-    message_prop = "Il tuo pacco è stato inviato a " + str(data_neig[1]) + " " + str(data_neig[2]) + " in " + str(data_neig[3])
-    
-    #send messages to bot telegram
-    print(message_neighbour)
-    print(message_prop)
+    send_message_bot(data_prop, data_neig)
 
     connection.close()
     message_ringdoor = str(data_neig[0])
 
     return message_ringdoor
+
 
 
 class requestHttpReceiver (BaseHTTPRequestHandler):
@@ -64,9 +79,8 @@ class requestHttpReceiver (BaseHTTPRequestHandler):
 
 
 def run():   
-  server_address = ('127.0.0.1', 8081)  
-  httpd = HTTPServer(server_address, requestHttpReceiver)  
-  print('http server is running...')  
+  server_address = ('127.0.0.1', port_server)  
+  httpd = HTTPServer(server_address, requestHttpReceiver)   
   httpd.serve_forever() 
 
 
