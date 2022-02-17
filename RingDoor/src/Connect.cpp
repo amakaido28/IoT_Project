295b8b29f8d093e374d3504a59a5197bed62b351
Lorenzo Calvano <80829330+Lorenzo-Calvano@users.noreply.github.com>
2022-02-17 18:24:19 +0100

    Update Connect.cpp

--- RingDoor/src/Connect.cpp
+++ RingDoor/src/Connect.cpp
@@ -2,8 +2,8 @@
 #include <WiFi.h>
 #include <Connect.h>
 
-const char* ssid = "iPhone Lorenzo";
-const char* password = "12345678";
+const char* ssid = "Wi-Fi";
+const char* password = "Password";
 
 const uint16_t port = 5050;
 const char* host = "172.20.10.2";
