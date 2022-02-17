d0ee3d5b858e05f41588d22b7a9b69f33a36e7bf
Lorenzo Calvano <80829330+Lorenzo-Calvano@users.noreply.github.com>
2022-02-17 19:02:52 +0100

    Delete ex.py

--- ex.py
+++
@@ -1,9 +0,0 @@
-import http.client as http
-
-
-conn = http.HTTPConnection("127.0.0.1:8081")
-conn.request("GET", "/Receiver", "00:00:5e:00:53:af")
-response = conn.getresponse()
-msg = response.read(17).decode('utf-8')
-conn.close()
-print(msg)
